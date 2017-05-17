f99ff59ad06bdc55876e067edaff0ab345f4d80d
Ematinovkey <Ematinovkey@gmail.com>
2017-05-17 18:58:58 +0300

    Add project files.

---
+++ 2048(WinAPI)/2048(WinAPI).cpp
@@ -0,0 +1,320 @@
+// 2048(WinAPI).cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "2048(WinAPI).h"
+#include <wingdi.h>
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+int  _x, _y;
+bool started;
+char buff[4];
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+GameAI current;
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_MY2048WINAPI, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY2048WINAPI));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY2048WINAPI));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MY2048WINAPI);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Store instance handle in our global variable
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		350,50, 800, 800, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static HFONT numF = CreateFont(80, 40, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, "Clear Sans");
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case ID_MENU_NEWGAME:
+			current.generateNew();
+			started = true;
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_KEYDOWN:
+	{
+		if (started)
+		{
+			switch (wParam)
+			{
+			case VK_LEFT:
+				current.left();
+				break;
+			case VK_UP:
+				current.up();
+				break;
+			case VK_RIGHT:
+				current.right();
+				break;
+			case VK_DOWN:
+				current.down();
+				break;
+			}
+			InvalidateRect(hWnd, NULL, TRUE);
+			if (current.is_full())
+			{
+				MessageBox(hWnd, "Game Over", "You lose!", MB_OK);
+				started = false;
+			}
+			else if (current.win())
+			{
+				started = false;
+
+				MessageBox(hWnd, "Game Over", "You win!", MB_OK);
+
+			}
+
+			else if(!current.is_pseudo_full())
+				current.random_spawn();
+		}
+		break;
+	}
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		RECT rc, tile;
+		GetClientRect(hWnd, &rc);
+		_x = rc.right / 4, _y = rc.bottom / 4;
+		char buff[5];
+		HDC hdc, hdcMem;
+		HBITMAP hbmMem;
+		HGDIOBJ hbmOld;
+		hdc = BeginPaint(hWnd, &ps);
+		hdcMem = CreateCompatibleDC(hdc);
+		hbmMem = CreateCompatibleBitmap(hdc, rc.right - rc.left, rc.bottom - rc.top);
+		hbmOld = SelectObject(hdcMem, hbmMem);
+		FillRect(hdcMem, &rc, (HBRUSH)(COLOR_WINDOW + 1));
+		SetBkMode(hdcMem, TRANSPARENT);
+		SelectObject(hdcMem, numF);
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+			{
+				int val = current.get_tile_value(i, j);
+				SetRect(&tile, i*_x, j*_y, (i + 1)*_x, (j + 1)*_y);
+				SetTextColor(hdcMem, RGB(249, 246, 242));
+				switch (val)
+				{
+				case 0:
+					SelectObject(hdcMem, GetStockObject(WHITE_BRUSH));
+					break;
+				case 2:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(238, 228, 218)));
+					SetTextColor(hdcMem, RGB(119, 110, 101));
+					break;
+				case 4:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(237, 224, 200)));
+					SetTextColor(hdcMem, RGB(119, 110, 101));
+					break;
+				case 8:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(242, 177, 121)));
+					break;
+				case 16:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(245, 149, 99)));
+					break;
+				case 32:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(246, 124, 95)));
+					break;
+				case 64:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(246, 94, 59)));
+					break;
+				case 128:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(237, 207, 114)));
+					break;
+				case 256:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(237, 204, 97)));
+					break;
+				case 512:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(237, 200, 80)));
+					break;
+				case 1024:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(237, 197, 63)));
+					break;
+				case 2048:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(237, 194, 46)));
+					break;
+				default:
+					SelectObject(hdcMem, CreateSolidBrush(RGB(60, 58, 50)));
+					break;
+				}
+				SelectObject(hdcMem, GetStockObject(WHITE_PEN));
+				Rectangle(hdcMem, i*_x, j*_y, (i + 1)*_x, (j + 1)*_y);
+				_itoa(val, buff, 10);
+				if(val)
+					DrawText(hdcMem, buff, -1, &tile, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
+			}
+		BitBlt(hdc,
+			rc.left, rc.top,
+			rc.right, rc.bottom,
+			hdcMem,
+			rc.left, rc.top,
+			SRCCOPY);
+		SelectObject(hdcMem, hbmOld);
+		DeleteObject(hbmMem);
+		DeleteDC(hdcMem);
+		EndPaint(hWnd, &ps);
+	}
+	break;
+
+	case WM_ERASEBKGND:
+		return (LRESULT)1;
+
+	case WM_GETMINMAXINFO:
+	{
+		LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
+		lpMMI->ptMinTrackSize.x = 800;
+		lpMMI->ptMinTrackSize.y = 800;
+		lpMMI->ptMaxTrackSize.x = 800;
+		lpMMI->ptMaxTrackSize.y = 800;
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ 2048(WinAPI)/Kernel.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <ctime>
+
+using namespace std;
+
+inline int random_index(int x)
+{
+	int index;
+	index = rand() % x;
+	return index;
+}
+
+class GameAI
+{
+	int field[4][4];
+	int b_field[4][4];
+	int win_n = 2048;//only deg of 2;
+
+	void fall(int *line)
+	{
+		int temp[4] = { 0,0,0,0 }, k = 0;
+		for (int i = 0; i < 4; i++)
+			if (line[i] != 0)
+				temp[k++] = line[i];
+		for (int i = 0; i < 4; i++)
+			line[i] = temp[i];
+	}
+	void mov(int *line)
+	{
+		fall(line);
+		for (int i = 0; i < 3; i++)
+			if (line[i] == line[i + 1])
+			{
+				line[i] *= 2;
+				line[i + 1] = 0;
+				fall(line);
+			}
+	}
+	int max_n()
+	{
+		int max = 0;
+		for (int i = 0; i<4; i++)
+			for (int j = 0; j<4; j++)
+				if (field[i][j]>max)
+					max = field[i][j];
+		return max;
+	}
+public:
+
+	GameAI()
+	{
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				field[i][j] = 0;
+	}
+
+	void generateNew()
+	{
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				field[i][j] = 0;
+		srand(time(NULL));
+		int i = 0, j = 0;
+		i = random_index(4);
+		j = random_index(4);
+		field[i][j] = 2;
+		i = random_index(4);
+		j = random_index(4);
+		field[i][j] = 2;
+
+	}
+	bool win()
+	{
+		if (max_n() == win_n)
+			return true;
+		return false;
+	}
+
+	void left()
+	{
+		int temp[4];
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+				temp[j] = field[j][i];
+			mov(temp);
+			for (int j = 0; j < 4; j++)
+				field[j][i] = temp[j];
+		}
+	}
+	void right()
+	{
+		int temp[4];
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+				temp[j] = field[3 - j][i];
+			mov(temp);
+			for (int j = 0; j < 4; j++)
+				field[3 - j][i] = temp[j];
+		}
+	}
+	void up()
+	{
+		for (int i = 0; i < 4; i++)
+			mov(field[i]);
+	}
+	void down()
+	{
+		int temp[4];
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+				temp[j] = field[i][3 - j];
+			mov(temp);
+			for (int j = 0; j < 4; j++)
+				field[i][3 - j] = temp[j];
+		}
+	}
+	void random_spawn()
+	{
+		int i, j, k;
+		i = random_index(4);
+		j = random_index(4);
+		k = random_index(10);
+		if (field[i][j] == 0)
+		{
+			if (k == 2)
+				field[i][j] = 4;
+			else
+				field[i][j] = 2;
+		}
+		else
+			random_spawn();
+	}
+	bool is_pseudo_full()
+	{
+		bool check = true;
+		for (int i = 0; i<4; i++)
+			for (int j = 0; j<4; j++)
+				if (field[i][j] == 0)
+					check = false;
+		return check;
+	}
+	bool is_full()
+	{
+		bool check = is_pseudo_full();
+		if (check)
+		{
+			int ch_field[4][4];
+			memcpy(ch_field, field, 64);
+			up();
+			down();
+			left();
+			right();
+			if (memcmp(ch_field, field, 64))
+			{
+				check = false;
+				memcpy(field, ch_field, 64);
+			}
+		}
+		return check;
+	}
+
+	int get_tile_value(int st_ind, int col_ind)
+	{
+		return field[st_ind][col_ind];
+	}
+};
